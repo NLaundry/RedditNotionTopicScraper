ab4cbeee7ddd97317b2162c1d5e4eb5a9cd43bcf
Nathan Laundry <nathanlaundry@rogers.com>
2022-01-30 16:14:12 -0500

    added keywords

--- index.mjs
+++ index.mjs
@@ -1,7 +1,5 @@
 import { Client } from "@notionhq/client"
 import fetch from 'node-fetch'
-import Request from 'request'
-import Headers from 'request'
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.NOTION_DATABASE_ID
@@ -23,23 +21,37 @@ async function get_reddit(subreddit, listing, limit, timeframe) {
 	return posts
 }
 
+function flatten_string_array(arr){
+	let str = ""
+	for (let item in arr) {
+		str += `${arr[item]} `
+	}
+	return str
+}
+
 async function parse_posts(posts) {
 	let parsed_posts = []
 	for (let post of posts) {
 		let data = post.data
 		let summary = ""
+		let keywords = ""
 		try {
 			summary = await get_summary(data.url, summary_length, summary_keywords)
+			
+			keywords = flatten_string_array(summary.sm_api_keyword_array)
 			summary = summary.sm_api_content
 		} finally {
 		}
+
 		let entry = {
 			"title": data.title,
 			"url": data.url,
 			"summary": summary,
+			"keywords": keywords,
 		}
 		parsed_posts.push(entry)
 	}
+	// console.log(parsed_posts)
 	return parsed_posts
 }
 
@@ -50,7 +62,7 @@ async function get_summary(url, length, keywordCount){
 	return data
 }
 
-async function addItem(title, url, summary) {
+async function addItem(title, url, summary, keywords) {
   try {
 	const entry = {
       "parent": { "database_id": databaseId },
@@ -84,6 +96,17 @@ async function addItem(title, url, summary) {
 					},
 				],
 			},
+			keywords: {
+				rich_text: [
+					{
+						type: "text",
+						text: {
+							content: keywords,
+						},
+						plain_text: keywords,
+					},
+				],
+			},
       },
     }
 
@@ -100,7 +123,8 @@ async function main(){
 	let parsed_posts = await parse_posts(posts)
 
 	for (let post of parsed_posts) {
-		await addItem(post.title, post.url, post.summary)
+		console.log(post)
+		await addItem(post.title, post.url, post.summary, post.keywords)
 	}
 }
 
